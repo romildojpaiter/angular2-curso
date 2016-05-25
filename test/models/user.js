fcb6fa5062d824ac8481aab77bb9976394d2a853
Romildo Jozué Paiter <romildo.paiter@sigma.com.br>
2016-05-25 18:12:18 -0400

    Seguindo tudorial page 29

---
+++ test/models/user.js
@@ -0,0 +1,12 @@
+var User = (function () {
+    function User(firstname, lastname) {
+        this.fullname = firstname + " " + lastname;
+    }
+    User.prototype.hello = function () {
+        return "Hello, " + this.fullname;
+    };
+    return User;
+}());
+var user = new User("Mary", "Jane");
+document.body.innerHTML = user.hello();
+//# sourceMappingURL=user.js.map
